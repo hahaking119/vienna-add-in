59fb19314b9284b3503ffa00fe7b2a1b98287dad
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-02-26 09:51:25 +0000

    Working on issue 37.  * Made some changes to the ICCRepository query interface.

--- VIENNAAddInUnitTests/upcc3/ccts/dra/DRACCRepositoryTest.cs
+++ VIENNAAddInUnitTests/upcc3/ccts/dra/DRACCRepositoryTest.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using VIENNAAddIn.upcc3.ccts;
 using VIENNAAddIn.upcc3.ccts.dra;
 using VIENNAAddInUnitTests.upcc3.XSDGenerator.Generator.TestRepository;
-using System.Linq;
 
 namespace VIENNAAddInUnitTests.upcc3.ccts.dra
 {
@@ -12,13 +12,36 @@ namespace VIENNAAddInUnitTests.upcc3.ccts.dra
     public class DRACCRepositoryTest
     {
         [Test]
+        public void TestCreateBDT()
+        {
+        }
+
+        [Test]
+        public void TestFindCDTs()
+        {
+            ICCRepository repository = new CCRepository(new TestEARepository1());
+            foreach (ICDTLibrary library in repository.Libraries<ICDTLibrary>())
+            {
+                IEnumerable<IGrouping<string, ICDT>> cdtByType = from cdt in library.CDTs group cdt by cdt.CON.Type;
+                foreach (var cdtGroup in cdtByType)
+                {
+                    Console.WriteLine(cdtGroup.Key);
+                    foreach (ICDT cdt in cdtGroup)
+                    {
+                        Console.WriteLine("  " + cdt.Name);
+                    }
+                }
+            }
+        }
+
+        [Test]
         public void TestReadAccess()
         {
             ICCRepository repository = new CCRepository(new TestEARepository1());
-            var libraries = new List<IBusinessLibrary>(repository.Libraries);
+            var libraries = new List<IBusinessLibrary>(repository.AllLibraries());
             Assert.AreEqual(3, libraries.Count);
 
-            var bLib1 = libraries[0];
+            IBusinessLibrary bLib1 = libraries[0];
             Assert.AreEqual("blib1", bLib1.Name);
             Assert.AreEqual(BusinessLibraryType.bLibrary, bLib1.Type);
             Assert.AreEqual("http://test/blib1", bLib1.BaseURN);
@@ -29,9 +52,9 @@ namespace VIENNAAddInUnitTests.upcc3.ccts.dra
             Assert.AreEqual("primlib1", primLib1.BaseURN);
             var prims = new List<IPRIM>(primLib1.PRIMs);
             Assert.AreEqual(2, prims.Count);
-            var stringType = prims[0];
+            IPRIM stringType = prims[0];
             Assert.AreEqual("String", stringType.Name);
-            var decimalType = prims[1];
+            IPRIM decimalType = prims[1];
             Assert.AreEqual("Decimal", decimalType.Name);
 
             var cdtLib1 = (ICDTLibrary) libraries[2];
@@ -40,39 +63,15 @@ namespace VIENNAAddInUnitTests.upcc3.ccts.dra
             Assert.AreEqual("cdtlib1", cdtLib1.BaseURN);
             var cdts = new List<ICDT>(cdtLib1.CDTs);
             Assert.AreEqual(2, cdts.Count);
-            var date = cdts[0];
+            ICDT date = cdts[0];
             Assert.AreEqual(stringType.Name, date.CON.Type);
             var dateSups = new List<IDTComponent>(date.SUPs);
             Assert.AreEqual(1, dateSups.Count);
-            var dateFormat = dateSups[0];
+            IDTComponent dateFormat = dateSups[0];
             Assert.AreEqual("Format", dateFormat.Name);
             Assert.AreEqual(stringType.Name, dateFormat.Type);
 
             // TODO check CDT Measure
-        }
-
-        [Test]
-        public void TestFindCDTsByName()
-        {
-            ICCRepository repository = new CCRepository(new TestEARepository1());
-            foreach (CDTLibrary cdtLibrary in repository.Libraries.Where(IsCDTLibrary))
-            {
-                foreach (var date in cdtLibrary.CDTs.Where(cdt => cdt.Name == "Date"))
-                {
-                    Console.WriteLine(date.CON.Type);
-                }
-            }
-        }
-
-        private static bool IsCDTLibrary(IBusinessLibrary l)
-        {
-            return l.Type == BusinessLibraryType.CDTLibrary;
-        }
-
-        [Test]
-        public void TestCreateBDT()
-        {
-            // TODO
         }
     }
 }
