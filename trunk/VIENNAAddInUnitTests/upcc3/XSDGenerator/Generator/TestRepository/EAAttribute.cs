89cbcf86cc8e2c553441758d9aeac7bf0c93e332
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-02-27 10:16:18 +0000

    Working on issue 37.  * Major refactoring of TestRepository (separated EA-interface implementations from repository building code).

---
+++ trunk/VIENNAAddInUnitTests/upcc3/XSDGenerator/Generator/TestRepository/EAAttribute.cs
@@ -0,0 +1,219 @@
+using System;
+using EA;
+using Attribute=EA.Attribute;
+
+namespace VIENNAAddInUnitTests.upcc3.XSDGenerator.Generator.TestRepository
+{
+    internal class EAAttribute : Attribute, IEACollectionElement
+    {
+        private readonly UpccAttribute attribute;
+
+        public EAAttribute(UpccAttribute attribute)
+        {
+            this.attribute = attribute;
+        }
+
+        #region Attribute Members
+
+        public bool Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastError()
+        {
+            throw new NotImplementedException();
+        }
+
+        string IDualAttribute.Name
+        {
+            get { return Name; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Visibility
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        string IDualAttribute.Stereotype
+        {
+            get { return attribute.Stereotype; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Containment
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsStatic
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsCollection
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsOrdered
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool AllowDuplicates
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        string IDualAttribute.LowerBound
+        {
+            get { return (attribute.LowerBound ?? "1"); }
+            set { throw new NotImplementedException(); }
+        }
+
+        string IDualAttribute.UpperBound
+        {
+            get { return (attribute.UpperBound ?? "1"); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Container
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Notes
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsDerived
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int AttributeID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Pos
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Length
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Precision
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Scale
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsConst
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Style
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int ClassifierID
+        {
+            get { return attribute.Type.Resolve().ElementID; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Default
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        string IDualAttribute.Type
+        {
+            get { return attribute.Type.Resolve().Name; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public Collection Constraints
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        Collection IDualAttribute.TaggedValues
+        {
+            get { return EACollection<EATaggedValue>.Wrap(attribute.TaggedValues); }
+        }
+
+        public string AttributeGUID
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string StyleEx
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public ObjectType ObjectType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int ParentID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string StereotypeEx
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public Collection TaggedValuesEx
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region IEACollectionElement Members
+
+        public string Name
+        {
+            get { return attribute.Name; }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/VIENNAAddInUnitTests/upcc3/XSDGenerator/Generator/TestRepository/EAConnector.cs
@@ -0,0 +1,268 @@
+using System;
+using EA;
+
+namespace VIENNAAddInUnitTests.upcc3.XSDGenerator.Generator.TestRepository
+{
+    internal class EAConnector : IEACollectionElement, Connector
+    {
+        private readonly UpccConnector connector;
+
+        public EAConnector(UpccConnector connector)
+        {
+            this.connector = connector;
+        }
+
+        #region Connector Members
+
+        public bool Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLastError()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ConnectorID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        string IDualConnector.Name
+        {
+            get { return Name; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Direction
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Notes
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Type
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Subtype
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int ClientID
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int SupplierID
+        {
+            get { return connector.PathToSupplier.Resolve().ElementID; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int SequenceNo
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        string IDualConnector.Stereotype
+        {
+            get { return connector.Stereotype; }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string VirtualInheritance
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string ConnectorGUID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsRoot
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsLeaf
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public bool IsSpec
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public Collection Constraints
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Collection TaggedValues
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ConnectorEnd ClientEnd
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ConnectorEnd SupplierEnd
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int RouteStyle
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string StyleEx
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string EventFlags
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string TransitionEvent
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string TransitionGuard
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string TransitionAction
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public ObjectType ObjectType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Color
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int Width
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public Collection CustomProperties
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int DiagramID
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string StereotypeEx
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int StartPointX
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int StartPointY
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int EndPointX
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public int EndPointY
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string StateFlags
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public Properties Properties
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string MetaType
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Alias
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string get_MiscData(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IEACollectionElement Members
+
+        public string Name
+        {
+            get { return connector.Name; }
+        }
+
+        #endregion
+    }
+}
