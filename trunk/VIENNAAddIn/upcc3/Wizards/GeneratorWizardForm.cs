4aec625e14f2804d1819e725c0fc7e958c85da6c
pichler.christian <pichler.christian@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-04-01 15:27:17 +0000

    updated the initial XML schema generator wizard to utilize the CC XML schema generator

--- trunk/VIENNAAddIn/upcc3/Wizards/GeneratorWizardForm.cs
+++ trunk/VIENNAAddIn/upcc3/Wizards/GeneratorWizardForm.cs
@@ -23,6 +23,9 @@ namespace VIENNAAddIn.upcc3.Wizards
         private Cache cache;
         private string selectedBIVName;
         private string selectedDOCName;
+        private const int MARGIN = 15;
+        private int mouseDownPosX;
+        private string outputDirectory = "";        
 
         public GeneratorWizardForm(CCRepository ccRepository)
         {
@@ -97,7 +100,7 @@ namespace VIENNAAddIn.upcc3.Wizards
             // todo: extend PathIsValid 
             GatherUserInput();
 
-            cache.BIVs[selectedBIVName].LoadDOCs(ccR);
+            cache.BIVs[selectedBIVName].LoadDOCsInBIV(ccR);
 
             // model
             // todo: set default value for document model and select afterwards
@@ -108,8 +111,7 @@ namespace VIENNAAddIn.upcc3.Wizards
 
             foreach (cDOC doc in cache.BIVs[selectedBIVName].DOCs.Values)
             {
-                checkedlistboxDOCs.Items.Add(doc.Name, doc.State);
-                
+                checkedlistboxDOCs.Items.Add(doc.Name, doc.State);                
             }            
         }
 
@@ -125,6 +127,7 @@ namespace VIENNAAddIn.upcc3.Wizards
         {            
             selectedBIVName = comboBIVs.SelectedIndex >= 0 ? comboBIVs.SelectedItem.ToString() : "";
             selectedDOCName = checkedlistboxDOCs.SelectedIndex >= 0 ? checkedlistboxDOCs.SelectedItem.ToString() : "";
+            outputDirectory = textOutputDirectory.Text;
         }
 
         #endregion
@@ -147,6 +150,74 @@ namespace VIENNAAddIn.upcc3.Wizards
         {
             // todo: make path check
             MirrorDOCSettingsToUI();
+        }
+
+        private void checkedlistboxDOCs_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            GatherUserInput();
+
+            if (mouseDownPosX > MARGIN)
+            {
+                e.NewValue = e.CurrentValue;
+            }
+            else
+            {
+                cache.BIVs[selectedBIVName].DOCs[selectedDOCName].State = e.NewValue;
+            } 
+        }
+
+        private void checkedlistboxDOCs_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                mouseDownPosX = e.X;
+            else
+                mouseDownPosX = -1;
+        }
+
+        private void buttonBrowseFolders_Click(object sender, EventArgs e)
+        {
+            GatherUserInput();
+            
+            FolderBrowserDialog browseDialog = new FolderBrowserDialog();
+
+            browseDialog.Description = "Select the directory where the generated XML schema files are stored:";
+
+            if (!(outputDirectory.Equals("")))
+            {
+                browseDialog.SelectedPath = outputDirectory;
+            }
+            
+            DialogResult dialogResult = browseDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                textOutputDirectory.Text = browseDialog.SelectedPath;
+            }
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            richtextStatus.Text = "";
+
+            GatherUserInput();
+
+            cBIV currentBIV = cache.BIVs[selectedBIVName];
+
+            IDOCLibrary docl = (IDOCLibrary)ccR.GetLibrary(currentBIV.Id);
+
+            // TODO: xsd generator needs to be adapted - currently all doc libraries are being generated whereas
+            // only the ones that are checked should be generated.. 
+
+            //TODO: currently the wizard just takes the input from the text fields whereas the prefix and the
+            // target namespace should be (a) stored in the cache and (b) read from there while generation.. 
+            XSDGenerator.Generator.XSDGenerator.GenerateSchemas(ccR, docl, textTargetNS.Text, textPrefixTargetNS.Text, checkboxAnnotations.CheckState == CheckState.Checked ? true : false, outputDirectory);
+
+            //VIENNAAddIn.upcc3.XSDGenerator.Generator.XSDGenerator.GenerateSchemas(ccRepository, docLibrary,
+            //                                                                                  "urn:test:namespace", "test", true,
+            //                                                                                  PathToTestResource(
+            //                                                                                      "\\XSDGeneratorTest\\all"));
+
+            richtextStatus.Text = "Generating XML schemas completed!";
         }
     }
 }
