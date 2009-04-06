9198e79f13a7910514a6dbd5192332128662156e
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-04-06 13:11:38 +0000

    Issue 44:  * Added AggregationKind to IASBIE.

--- trunk/VIENNAAddIn/upcc3/ccts/util/AggregationKind.cs
+++ trunk/VIENNAAddIn/upcc3/ccts/util/AggregationKind.cs
@@ -1,9 +1,9 @@
 namespace VIENNAAddIn.upcc3.ccts.util
 {
-    public static class AggregationKind
+    public enum AggregationKind
     {
-        public const int None = 0;
-        public const int Shared = 1;
-        public const int Composite = 2;
+        None,
+        Shared,
+        Composite
     }
 }
---
+++ trunk/VIENNAAddInUnitTests/upcc3/ccts/util/AggregationKindTest.cs
@@ -0,0 +1,28 @@
+using System;
+using NUnit.Framework;
+using VIENNAAddIn.upcc3.ccts.util;
+
+namespace VIENNAAddInUnitTests.upcc3.ccts.util
+{
+    [TestFixture]
+    public class AggregationKindTest
+    {
+        [Test]
+        public void TestEnumFromInt()
+        {
+            Assert.AreEqual(AggregationKind.None, (AggregationKind)Enum.ToObject(typeof(AggregationKind), 0));
+            Assert.AreEqual(AggregationKind.Shared, Enum.ToObject(typeof(AggregationKind), 1));
+            Assert.AreEqual(AggregationKind.Composite, Enum.ToObject(typeof(AggregationKind), 2));
+            Assert.AreEqual(false, Enum.IsDefined(typeof (AggregationKind), -1));
+            Assert.AreEqual(false, Enum.IsDefined(typeof (AggregationKind), 3));
+        }
+
+        [Test]
+        public void TestEnumToInt()
+        {
+            Assert.AreEqual(0, (int) AggregationKind.None);
+            Assert.AreEqual(1, (int) AggregationKind.Shared);
+            Assert.AreEqual(2, (int) AggregationKind.Composite);
+        }
+    }
+}
