763168ca1f137a803c8ed7307034b313c739f9f6
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-04-22 12:38:41 +0000

    Implemented extension method for dictionaries for debugging dictionary content.

--- VIENNAAddIn/Utils/DictionaryExtensions.cs
+++ VIENNAAddIn/Utils/DictionaryExtensions.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace VIENNAAddIn.Utils
 {
@@ -26,6 +29,62 @@ namespace VIENNAAddIn.Utils
             }
             return value;
         }
-    
+
+        ///<summary>
+        /// Print the dictionary to Console.Out in tabular form.
+        ///</summary>
+        ///<param name="dictionary"></param>
+        public static void PrintAsTable<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            dictionary.PrintAsTable(Console.Out);
+        }
+
+        ///<summary>
+        /// Print the dictionary to the writer in tabular form.
+        ///</summary>
+        ///<param name="dictionary"></param>
+        ///<param name="writer"></param>
+        ///<typeparam name="TKey"></typeparam>
+        ///<typeparam name="TValue"></typeparam>
+        public static void PrintAsTable<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TextWriter writer)
+        {
+            int keyColumnWidth = "Key".Length;
+            foreach (var key in dictionary.Keys)
+            {
+                keyColumnWidth = Math.Max(keyColumnWidth, key.ToString().Length);
+            }
+            int valueColumnWidth = "Value".Length;
+            foreach (var value in dictionary.Values)
+            {
+                valueColumnWidth = Math.Max(valueColumnWidth, value.ToString().Length);
+            }
+            var hr = "+" + new String('-', keyColumnWidth + valueColumnWidth + 5) + "+";
+            writer.WriteLine(hr);
+            writer.WriteLine("| {0," + (-keyColumnWidth) + "} | {1," + (-valueColumnWidth) + "} |", "Key", "Value");
+            writer.WriteLine(hr);
+            foreach (var key in dictionary.Keys)
+            {
+                writer.WriteLine("| {0," + (-keyColumnWidth) + "} | {1," + (-valueColumnWidth) + "} |", key, dictionary[key]);
+            }
+            writer.WriteLine(hr);
+        }
+
+        ///<summary>
+        ///</summary>
+        ///<param name="dictionary"></param>
+        ///<typeparam name="TKey"></typeparam>
+        ///<typeparam name="TValue"></typeparam>
+        ///<returns></returns>
+        public static string ToStringDebug<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            var result = new StringBuilder();
+            result.Append("[");
+            foreach (var key in dictionary.Keys)
+            {
+                result.Append(string.Format("({0} => {1})", key, dictionary[key]));
+            }
+            result.Append("]");
+            return result.ToString();
+        }
     }
 }
