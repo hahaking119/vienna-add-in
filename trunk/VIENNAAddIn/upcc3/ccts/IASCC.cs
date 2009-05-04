22b0089f7d3cbe052948d0a5e81eb2b6df30827e
michael.strommer@researchstudio.at <michael.strommer@researchstudio.at@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-05-04 13:12:43 +0000

    AggregationKind for ACCs introduced.

--- trunk/VIENNAAddIn/upcc3/ccts/IASCC.cs
+++ trunk/VIENNAAddIn/upcc3/ccts/IASCC.cs
@@ -6,11 +6,14 @@
 // For further information on the VIENNAAddIn project please visit 
 // http://vienna-add-in.googlecode.com
 // *******************************************************************************
+using VIENNAAddIn.upcc3.ccts.util;
+
 namespace VIENNAAddIn.upcc3.ccts
 {
     public interface IASCC : ICC, ISequenced, IHasMultiplicity
     {
         IACC AssociatingElement { get; }
         IACC AssociatedElement { get; }
+        AggregationKind AggregationKind { get; }
     }
 }
