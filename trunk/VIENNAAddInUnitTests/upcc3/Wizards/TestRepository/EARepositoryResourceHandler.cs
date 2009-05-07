ffd2a6f347b07adeca549261f0359900a76a97a8
pichler.christian <pichler.christian@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-05-07 09:46:52 +0000

    Finalized implementation of the default UPCC3 model creator supporting dynamic load of XMI files hosted on the web as well as initial implementation of the appropriate Unit Test Cases;

--- trunk/VIENNAAddInUnitTests/upcc3/Wizards/TestRepository/EARepositoryResourceHandler.cs
+++ trunk/VIENNAAddInUnitTests/upcc3/Wizards/TestRepository/EARepositoryResourceHandler.cs
@@ -5,7 +5,7 @@ using VIENNAAddInUnitTests.TestRepository;
 namespace VIENNAAddInUnitTests.upcc3.Wizards.TestRepository
 {
     [TestFixture]
-    public class EARepositoryModelCreator : EARepository
+    public class EARepositoryResourceHandler : EARepository
     {
         [Test]
         public void Test1()
