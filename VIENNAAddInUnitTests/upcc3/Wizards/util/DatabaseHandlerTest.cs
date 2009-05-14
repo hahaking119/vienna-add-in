74a37b8b93439bd18206f4df84e3d504b4b109b5
pichler.christian <pichler.christian@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-05-14 17:07:02 +0000

    added skeletons for future DB2 functionality

---
+++ VIENNAAddInUnitTests/upcc3/Wizards/util/DatabaseHandlerTest.cs
@@ -0,0 +1,30 @@
+// *******************************************************************************
+// This file is part of the VIENNAAddIn project
+// 
+// Licensed under GNU General Public License V3 http://gplv3.fsf.org/
+// 
+// For further information on the VIENNAAddIn project please visit 
+// http://vienna-add-in.googlecode.com
+// *******************************************************************************
+
+using NUnit.Framework;
+
+namespace VIENNAAddInUnitTests.upcc3.Wizards.util
+{
+    [TestFixture]
+    public class DatabaseHandlerTest
+    {
+        #region Test Settings
+
+        #endregion
+
+        [Test]
+        public void UselessTest()
+        {
+        }
+
+        # region Private Class Methods
+        
+        #endregion
+    }
+}
