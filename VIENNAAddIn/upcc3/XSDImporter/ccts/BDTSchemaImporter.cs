de4ce4225b8fe49e725dee82cf99d236759af8ad
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-05-18 08:43:58 +0000

    Fixed NullReferenceException concerning CON.BasicType Added XSDSimpleType Support Test on simpleXSDs completed successfully

--- VIENNAAddIn/upcc3/XSDImporter/ccts/BDTSchemaImporter.cs
+++ VIENNAAddIn/upcc3/XSDImporter/ccts/BDTSchemaImporter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Xml.Schema;
 using VIENNAAddIn.upcc3.ccts;
 using VIENNAAddIn.upcc3.ccts.dra;
@@ -21,6 +19,9 @@ namespace VIENNAAddIn.upcc3.XSDImporter.ccts
                 if(currentSchemaInfo.FileName.StartsWith("BusinessDataType"))
                 {
                     XmlSchema currentSchema = currentSchemaInfo.Schema;
+                    BDTLibrary bdtl = repo.Libraries<BDTLibrary>().ElementAt(0);
+                    PRIMLibrary primlib = repo.Libraries<PRIMLibrary>().ElementAt(0);
+
                     foreach (var currentElement in currentSchema.Items)
                     {
                         if (currentElement.GetType().Name == "XmlSchemaComplexType")
@@ -31,30 +32,32 @@ namespace VIENNAAddIn.upcc3.XSDImporter.ccts
                                 (XmlSchemaSimpleContentExtension) simpleContent.Content;
                             String baseTypeName = ConvertXSDType(simpleContentExtension.BaseTypeName.Name);
                             
-                            BDTLibrary bdtl = repo.Libraries<BDTLibrary>().ElementAt(0);
-                            PRIMLibrary primlib = repo.Libraries<PRIMLibrary>().ElementAt(0);
-
-                            
                             CONSpec conSpec = new CONSpec();
-                            conSpec.Name = baseTypeName;
-                            
+                         
+                            foreach (IPRIM primElement in primlib.Elements)
+                            {
+                                if(primElement.Name == baseTypeName)
+                                {
+                                    Console.WriteLine("Found matching PRIM Element: "+primElement.Name);
+                                    conSpec.BasicType = primElement;
+                                }
+                            }
                             BDTSpec bdtSpec = new BDTSpec();
-                            Debug.WriteLine("Found ComplexType: " + complexType.Name.Replace("Type", "").Replace(baseTypeName, ""));
+                            //Debug.WriteLine("Found ComplexType: " + complexType.Name.Replace("Type", "").Replace(baseTypeName, ""));
                             bdtSpec.Name = complexType.Name.Replace("Type", "").Replace(ConvertXSDType(baseTypeName), "");
                             bdtSpec.CON = conSpec;
-                            Console.WriteLine(complexType.Attributes.Count);
                             foreach (XmlSchemaAttribute attribute in simpleContentExtension.Attributes)
                             {
-                                Console.WriteLine("Found Attribute: " + attribute.Name.Replace(baseTypeName, ""));
+                                //Console.WriteLine("Found Attribute: " + attribute.Name.Replace(baseTypeName, ""));
                                 SUPSpec supSpec = new SUPSpec();
                                 supSpec.Name = attribute.Name.Replace(baseTypeName, "");
 
-                                Console.WriteLine("Schema Type Name: " + attribute.SchemaTypeName);
+                                //Console.WriteLine("Schema Type Name: " + attribute.SchemaTypeName);
                                 foreach (var primitiveType in primlib.Elements)
                                 {
                                     if (primitiveType.Name == ConvertXSDType(attribute.SchemaTypeName.ToString().Replace("http://www.w3.org/2001/XMLSchema", "")))
                                     {
-                                        Console.WriteLine("Found PrimitiveType: "+primitiveType.Name);
+                                        //Console.WriteLine("Found PrimitiveType: "+primitiveType.Name);
                                         supSpec.BasicType = primitiveType;
                                     }
                                 }
@@ -68,10 +71,20 @@ namespace VIENNAAddIn.upcc3.XSDImporter.ccts
                         if(currentElement.GetType().Name == "XMLSchemaSimpleType")
                         {
                             XmlSchemaSimpleType simpleType = (XmlSchemaSimpleType) currentElement;
-                            Console.WriteLine("Found SimpleType: "+simpleType.Name);
-                            //XmlSchemaSimpleContentExtension simpleContentExtension = (XmlSchemaSimpleContentExtension) simpleType.Content;
-
-
+                            XmlSchemaSimpleTypeRestriction simpleContent = (XmlSchemaSimpleTypeRestriction) simpleType.Content;
+                            String baseTypeName = ConvertXSDType(simpleContent.BaseTypeName.Name);
+                            BDTSpec bdtSpec = new BDTSpec();
+                            bdtSpec.Name = simpleType.Name.Replace("Type", "").Replace(ConvertXSDType(baseTypeName), "");
+                            CONSpec conSpec = new CONSpec();
+                            foreach (IPRIM primElement in primlib.Elements)
+                            {
+                                if (primElement.Name == baseTypeName)
+                                {
+                                    conSpec.BasicType = primElement;
+                                }
+                            }
+                            bdtSpec.CON = conSpec;
+                            bdtl.CreateElement(bdtSpec);
                         }
                     }
                 }
