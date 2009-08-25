b2a78435e21986ce4e3baabafa39502ae2f3b1b4
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-08-25 11:11:39 +0000

    Refactored on-the-fly validation service.

--- VIENNAAddIn/upcc3/ccts/otf/BInformationV.cs
+++
@@ -1,9 +0,0 @@
-namespace VIENNAAddIn.upcc3.ccts.otf
-{
-    public class BInformationV : AbstractEAPackage
-    {
-        public BInformationV(ItemId id, string name, ItemId parentId) : base(id, name, parentId)
-        {
-        }
-    }
-}
--- VIENNAAddIn/upcc3/ccts/otf/EAModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VIENNAAddIn.upcc3.ccts.otf
-{
-    public class EAModel : AbstractEAPackage
-    {
-        public EAModel(ItemId id, string name, ItemId parentId) : base(id, name, parentId)
-        {
-        }
-    }
-}
