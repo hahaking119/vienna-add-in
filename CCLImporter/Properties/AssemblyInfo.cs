cd857443da54bd4a8ed73ae8ef0f119d5c54ed7d
andi.kuntner <andi.kuntner@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-09-15 15:22:17 +0000

    renamed CCLImporter to CCLConverter

--- CCLImporter/Properties/AssemblyInfo.cs
+++ CCLImporter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CCLImporter")]
+[assembly: AssemblyTitle("CCLConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Research Studios Austria")]
-[assembly: AssemblyProduct("CCLImporter")]
+[assembly: AssemblyProduct("CCLConverter")]
 [assembly: AssemblyCopyright("Copyright © Research Studios Austria 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
