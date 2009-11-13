57cde3a55229314ddc8e773bba00429065b4ebe5
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-11-13 13:54:31 +0000

    Update issue 96
     * Finished first version of XSD Importer UI.
     * Connected UI to importer.
    
    Things left to do:
    
     * Input fields for remaining importer arguments (library names, qualifier, root doc element name).
     * User input error checking (e.g. mapping files must be mapping files).
     * Displaying errors/warnings to the user.
     * Update status display and progress bar during import.

--- trunk/VIENNAAddIn/upcc3/Wizards/ImporterWizard.xaml.cs
+++ trunk/VIENNAAddIn/upcc3/Wizards/ImporterWizard.xaml.cs
@@ -1,34 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Xml;
-using System.Xml.Schema;
+using EA;
 using VIENNAAddIn.menu;
 using VIENNAAddIn.upcc3.ccts.dra;
 using VIENNAAddIn.upcc3.import.cctsndr;
+using VIENNAAddIn.upcc3.import.ebInterface;
 
 namespace VIENNAAddIn.upcc3.Wizards
 {
     /// <summary>
-    /// Interaction logic for ImporterWizardForm.xaml
+    /// Interaction logic for ImporterWizard.xaml
     /// </summary>
     public partial class ImporterWizard : Window
     {
-        private int mappingfilesEbInterface = 1;
-        private Dictionary<string, TextBox> textboxes = new Dictionary<string, TextBox>();
-        private Dictionary<string, Button> buttons = new Dictionary<string, Button>();
-        private CCRepository CcRepository;
+        private readonly CCRepository ccRepository;
+        private readonly FileSelector cctsSchemaFileSelector;
+        private readonly FileSelector mappedSchemaFileSelector;
+        private readonly MultipleFilesSelector mappingFilesSelector;
 
-        public ImporterWizard(EA.Repository eaRepository)
+        public ImporterWizard(Repository eaRepository)
         {
+            ccRepository = new CCRepository(eaRepository);
+
             InitializeComponent();
-            CcRepository = new CCRepository(eaRepository);
-            buttons.Add("mappingFileB_1", this.mappingFileB_1);
-            textboxes.Add("mappingFileT_1", this.mappingFileT_1);
+
+            cctsSchemaFileSelector = new FileSelector(".xsc", "XML Schema files (.xsd)|*.xsd")
+                                     {
+                                         Width = 415,
+                                     };
+            cctsSchemaFileSelectorPanel.Children.Add(cctsSchemaFileSelector);
+
+            mappedSchemaFileSelector = new FileSelector(".xsc", "XML Schema files (.xsd)|*.xsd")
+                                       {
+                                           Width = 415,
+                                       };
+            mappedSchemaFileSelectorPanel.Children.Add(mappedSchemaFileSelector);
+
+            mappingFilesSelector = new MultipleFilesSelector(".*", "Mapping files|*.*")
+                                   {
+                                       Width = 415,
+                                   };
+            mappingFileSelectorScrollViewer.Content = mappingFilesSelector;
         }
 
         public static void ShowForm(AddInContext context)
@@ -36,118 +48,29 @@ namespace VIENNAAddIn.upcc3.Wizards
             new ImporterWizard(context.EARepository).Show();
         }
 
-        private string OpenFileDialog(string ext, string filter)
-        {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.DefaultExt = ext; // Default file extension
-            dlg.Filter = filter; // Filter files by extension
-            Nullable<bool> result = dlg.ShowDialog();
-           if (result == true)
-            {
-                return dlg.FileName;
-            }
-            return null;
-        }
-
-        private void AddControlMappingFiles()
-        {
-            gridEbInterface.Height += 100;
-            textboxes["mappingFileT_" + mappingfilesEbInterface].TextChanged -= new TextChangedEventHandler(textboxSelectMappingFile1_TextChanged);
-            mappingfilesEbInterface++;
-
-            Label newLbl = new Label();
-            newLbl.Content = "Select another mapping file:";
-            gridEbInterface.Children.Add(newLbl);
-            //Canvas.SetTop(newLbl, 130);
-            Canvas.SetLeft(newLbl, 5);
-
-            Button newBtn = new Button();
-            buttons.Add("mappingFileB_" + mappingfilesEbInterface, newBtn);
-            newBtn.Content = "...";
-            newBtn.Name = "mappingFileB_" + mappingfilesEbInterface;
-            newBtn.Width = 28;
-            newBtn.Height = 23;
-            gridEbInterface.Children.Add(newBtn);
-            //Canvas.SetTop(newBtn, 153);
-            Canvas.SetLeft(newBtn, 40);
-            newBtn.Click += new RoutedEventHandler(buttonBrowseFoldersMapping1_Click);
-
-            TextBox newTxt = new TextBox();
-            textboxes.Add("mappingFileT_" + mappingfilesEbInterface, newTxt);
-            newTxt.Name = "mappingFileT_" + mappingfilesEbInterface;
-            newTxt.Width = 291;
-            newTxt.Height = 23;
-            gridEbInterface.Children.Add(newTxt);
-            //Canvas.SetTop(newTxt, 153);
-            Canvas.SetLeft(newTxt, 0);
-            newTxt.TextChanged += new TextChangedEventHandler(textboxSelectMappingFile1_TextChanged);
-
-        }
-
-        private void textboxSelectMappingFile1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if(!string.IsNullOrEmpty(((TextBox)sender).Text))
-            {
-                AddControlMappingFiles();
-            }
-        }
-        
-        private void buttonBrowseFolders_Click(object sender, RoutedEventArgs e)
-        {
-            string filename = OpenFileDialog(".xsc", "XML Schema files (.xsd)|*.xsd");
-            if (!string.IsNullOrEmpty(filename))
-            {
-                textboxRootSchema.Text = filename;
-            }
-        }
-
-        private void buttonBrowseFoldersMapping1_Click(object sender, RoutedEventArgs e)
-        {
-            string filename = OpenFileDialog(".*", "Mapping files|*.*");
-            if (!string.IsNullOrEmpty(filename))
-            {
-                string receiver = "mappingFileT_" + ((Button)sender).Name.Split('_')[1];
-                textboxes[receiver].Text = filename;
-            }
-        }
-
-        private void buttonBrowseFolders2_Click(object sender, RoutedEventArgs e)
-        {
-            string filename = OpenFileDialog(".xsc", "XML Schema files (.xsd)|*.xsd");
-            if (!string.IsNullOrEmpty(filename))
-            {
-                textboxEbInterfaceSchema.Text = filename;
-            }
-        }
-
         private void buttonClose_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void buttonImport_Click(object sender, RoutedEventArgs e)
         {
+            Cursor = Cursors.Wait;
+            buttonImport.Visibility = Visibility.Collapsed;
+
             switch (tabControl1.SelectedIndex)
             {
-                case 0: // CCTS
-                    Cursor = Cursors.Wait;
-
-                    buttonImport.Visibility = Visibility.Collapsed;
-
-                    var context = new ImporterContext(CcRepository, textboxRootSchema.Text);
-                    import.cctsndr.XSDImporter.ImportSchemas(context);
-
-                    progressBar.Minimum = 0;
-                    progressBar.Maximum = 100;
-                    progressBar.Value = 100;
-
-                    textboxStatus.Text += "Importing the XML schema named \"" + context.RootSchemaFileName + "\" completed!\n";
-
-                    Cursor = Cursors.Arrow;
-                break;
-                case 1: // ebInterface
-                break;
+                case 0: // ebInterface
+                    new MappingImporter(mappingFilesSelector.FileNames, "ebInterface Invoice", "ebInterface", "ebInterface Types", "ebInterface", "Invoice").ImportMapping(ccRepository);
+                    break;
+                case 1: // CCTS
+                    XSDImporter.ImportSchemas(new ImporterContext(ccRepository, cctsSchemaFileSelector.FileName));
+                    break;
             }
+
+            progressBar.Value = 100;
+            textboxStatus.Text += "Import completed!\n";
+            Cursor = Cursors.Arrow;
         }
     }
 }
