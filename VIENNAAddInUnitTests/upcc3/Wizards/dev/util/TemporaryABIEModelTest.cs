9131400705014edf5d3e46f4d958a71b96f1ccb0
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-11-16 15:04:48 +0000

    added TemporaryABIEModelTest

---
+++ VIENNAAddInUnitTests/upcc3/Wizards/dev/util/TemporaryABIEModelTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using VIENNAAddIn.upcc3.ccts.dra;
+using VIENNAAddIn.upcc3.Wizards.dev.cache;
+using VIENNAAddIn.upcc3.Wizards.dev.util;
+using VIENNAAddInUnitTests.upcc3.Wizards.dev.TestRepository;
+using Assert=Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace VIENNAAddInUnitTests.upcc3.Wizards.dev.util
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class TemporaryABIEModelTest
+    {
+        private EARepositoryCCCache eaRepository;
+        private CCRepository ccRepository;
+        private TemporaryABIEModel temporaryABIEModel;
+        private CCCache ccCache;
+
+        #region Test SetUp/TearDown
+
+        [SetUp]
+        public void Setup()
+        {
+            eaRepository = new EARepositoryCCCache();
+            ccRepository = new CCRepository(eaRepository);
+            temporaryABIEModel = new TemporaryABIEModel();
+            ccCache = CCCache.GetInstance(ccRepository);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            eaRepository = null;
+            ccRepository = null;
+            temporaryABIEModel = null;
+        }
+
+        #endregion
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ShouldCreateBBIE()
+        {
+            List<string> BDTs = new List<string>();
+            temporaryABIEModel.createBBIE("underlyingBCCName", "BBIEName", BDTs);
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldCreateCDT()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldGeneratePotentialASBIEs()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldSelectASBIE()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldSelectBBIE()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldSelectBCC()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldSelectBDT()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void ShouldSetTargetACC()
+        {
+            ACC acc = ccCache.GetCCFromCCLibrary("cclib1", "Address");
+            temporaryABIEModel.SetTargetACC(acc);
+
+            Assert.Equals(acc, temporaryABIEModel.GetBasedOnACC());
+        }
+    }
+}
