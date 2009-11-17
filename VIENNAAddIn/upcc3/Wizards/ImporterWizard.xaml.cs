dd38b8a5e69f3edf138a00921c5b11ebef32d2c7
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-11-17 08:42:25 +0000

    Update issue 96  * Added Input fields for remaining importer arguments (library names, qualifier, root doc element name).

--- VIENNAAddIn/upcc3/Wizards/ImporterWizard.xaml.cs
+++ VIENNAAddIn/upcc3/Wizards/ImporterWizard.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using EA;
 using VIENNAAddIn.menu;
@@ -42,6 +44,8 @@ namespace VIENNAAddIn.upcc3.Wizards
                                        Width = 415,
                                    };
             mappingFileSelectorScrollViewer.Content = mappingFilesSelector;
+
+            buttonClose.Command = ApplicationCommands.Close;
         }
 
         public static void ShowForm(AddInContext context)
@@ -72,6 +76,39 @@ namespace VIENNAAddIn.upcc3.Wizards
             progressBar.Value = 100;
             textboxStatus.Text += "Import completed!\n";
             Cursor = Cursors.Arrow;
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public DelegateCommand(Action execute): this(execute, () => true)
+        {
+            this.execute = execute;
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
