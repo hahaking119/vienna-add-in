85c3b851d82835683a4971f5b3ae7eb337630c99
andi.kuntner <andi.kuntner@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-12-03 09:19:10 +0000

    created WPF GUI for BDT Editor

---
+++ trunk/VIENNAAddIn/upcc3/Wizards/dev/ui/BdtEditor.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using CctsRepository;
+using VIENNAAddIn.menu;
+using VIENNAAddIn.upcc3.Wizards.dev.temporarymodel.abiemodel;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+
+namespace VIENNAAddIn.upcc3.Wizards.dev.ui
+{
+    public partial class BdtEditor
+    {
+        public BdtEditor(ICctsRepository cctsRepository)
+        {
+            InitializeComponent(); 
+        }
+
+        public static void ShowForm(AddInContext context)
+        {
+            new BdtEditor(context.CctsRepository).Show();
+        }
+    }
+}
---
+++ trunk/VIENNAAddInUnitTests/upcc3/Wizards/dev/ui/BdtEditorFormTest.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System.Windows;
+using NUnit.Framework;
+using VIENNAAddIn.upcc3.ccts.dra;
+using VIENNAAddIn.upcc3.Wizards.dev.ui;
+using VIENNAAddInUnitTests.upcc3.Wizards.dev.TestRepository;
+
+namespace VIENNAAddInUnitTests.upcc3.Wizards.dev.ui
+{
+    [TestFixture]
+    public class BdtEditorFormTest
+    {
+        [Test]
+        [Ignore]
+        public void ShouldLaunchAndPopulateAbieModelerForm()
+        {
+            var t = new Thread(() => new Application().Run(new BdtEditor(new CCRepository(new EARepositoryABIEEditor()))));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+        }
+    }
+}
