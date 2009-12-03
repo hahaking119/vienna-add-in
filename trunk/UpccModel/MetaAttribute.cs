c720a5db0b38bdc70a9203728c6c650afbcb2d43
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-12-03 08:44:16 +0000

    Updates issue 110 - Renamed project/assembly to 'Upcc'. - Renamed UpccModel class to MetaModel. Therefore, the meta model is now accessed as Upcc.MetaModel.

--- trunk/UpccModel/MetaAttribute.cs
+++ trunk/UpccModel/MetaAttribute.cs
@@ -1,4 +1,4 @@
-namespace UpccModel
+namespace Upcc
 {
     public class MetaAttribute
     {
--- trunk/UpccModel/UpccPackageClassifierContainmentRelations.cs
+++ trunk/UpccModel/UpccPackageClassifierContainmentRelations.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace UpccModel
+namespace Upcc
 {
     public class UpccPackageClassifierContainmentRelations
     {
