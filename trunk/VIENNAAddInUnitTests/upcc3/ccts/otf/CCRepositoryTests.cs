22c4803a4ea598b26af5023554e371b0e43c0293
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-12-04 13:07:57 +0000

    Cleaning up.

--- trunk/VIENNAAddInUnitTests/upcc3/ccts/otf/CCRepositoryTests.cs
+++ trunk/VIENNAAddInUnitTests/upcc3/ccts/otf/CCRepositoryTests.cs
@@ -1,14 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using CctsRepository;
-using CctsRepository.BdtLibrary;
-using CctsRepository.BieLibrary;
-using CctsRepository.bLibrary;
-using CctsRepository.CcLibrary;
-using CctsRepository.CdtLibrary;
-using CctsRepository.DocLibrary;
-using CctsRepository.EnumLibrary;
-using CctsRepository.PrimLibrary;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using VIENNAAddIn;
