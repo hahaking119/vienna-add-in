4141bc85f01bcfb85769dc0e8ab6d9e0294ac371
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-12-07 13:15:05 +0000

    Update issue 113 - Finished re-implementation of BIELibrary and its elements with T4.

--- CctsRepository/BieLibrary/AbieSpec.cs
+++ CctsRepository/BieLibrary/AbieSpec.cs
@@ -1,3 +1,4 @@
+
 // *******************************************************************************
 // This file is part of the VIENNAAddIn project
 // 
@@ -6,79 +7,67 @@
 // For further information on the VIENNAAddIn project please visit 
 // http://vienna-add-in.googlecode.com
 // *******************************************************************************
+
 using System.Collections.Generic;
+// ReSharper disable RedundantUsingDirective
+using CctsRepository.BdtLibrary;
+using CctsRepository.BieLibrary;
+using CctsRepository.BLibrary;
 using CctsRepository.CcLibrary;
-using VIENNAAddInUtils;
+using CctsRepository.CdtLibrary;
+using CctsRepository.DocLibrary;
+using CctsRepository.EnumLibrary;
+using CctsRepository.PrimLibrary;
+// ReSharper restore RedundantUsingDirective
 
 namespace CctsRepository.BieLibrary
 {
-    public class AbieSpec
+    public partial class AbieSpec
     {
-        private List<AsbieSpec> asbies = new List<AsbieSpec>();
-        private List<BbieSpec> bbies = new List<BbieSpec>();
+		public string Name { get; set; }
 
-        public IEnumerable<BbieSpec> Bbies
-        {
-            get { return bbies; }
-            set { bbies = new List<BbieSpec>(value); }
-        }
+		public IAbie IsEquivalentTo { get; set; }
 
-        public IEnumerable<AsbieSpec> Asbies
-        {
-            get { return asbies; }
-            set { asbies = new List<AsbieSpec>(value); }
-        }
+		public IAcc BasedOn { get; set; }
 
-        public IAbie IsEquivalentTo { get; set; }
+		#region Tagged Values
 
-        public IAcc BasedOn { get; set; }
-        public IEnumerable<string> UsageRules { get; set; }
-        public string Name { get; set; }
-        public string DictionaryEntryName { get; set; }
-        public string Definition { get; set; }
-        public string UniqueIdentifier { get; set; }
-        public string VersionIdentifier { get; set; }
-        public string LanguageCode { get; set; }
-        public IEnumerable<string> BusinessTerms { get; set; }
+        ///<summary>
+        /// Tagged value 'businessTerm'.
+        ///</summary>
+		public IEnumerable<string> BusinessTerms { get; set; }
 
-        public static AbieSpec CloneAbie(IAbie abie)
-        {
-            return new AbieSpec
-                   {
-                       Name = abie.Name,
-                       DictionaryEntryName = abie.DictionaryEntryName,
-                       Definition = abie.Definition,
-                       UniqueIdentifier = abie.UniqueIdentifier,
-                       VersionIdentifier = abie.VersionIdentifier,
-                       LanguageCode = abie.LanguageCode,
-                       BusinessTerms = new List<string>(abie.BusinessTerms),
-                       UsageRules = new List<string>(abie.UsageRules),
-                       Bbies = abie.Bbies.Convert(bbie => BbieSpec.CloneBbie(bbie)),
-                       Asbies = abie.Asbies.Convert(asbie => new AsbieSpec(asbie)),
-                       IsEquivalentTo = abie.IsEquivalentTo,
-                       BasedOn = abie.BasedOn,
-                   }
-                ;
-        }
+        ///<summary>
+        /// Tagged value 'definition'.
+        ///</summary>
+		public string Definition { get; set; }
 
-        public void AddAsbie(AsbieSpec spec)
-        {
-            asbies.Add(spec);
-        }
+        ///<summary>
+        /// Tagged value 'dictionaryEntryName'.
+        ///</summary>
+		public string DictionaryEntryName { get; set; }
 
-        public void RemoveAsbie(string name)
-        {
-            asbies.RemoveAll(asbie => asbie.Name == name);
-        }
+        ///<summary>
+        /// Tagged value 'languageCode'.
+        ///</summary>
+		public string LanguageCode { get; set; }
 
-        public void AddBbie(BbieSpec spec)
-        {
-            bbies.Add(spec);
-        }
+        ///<summary>
+        /// Tagged value 'uniqueIdentifier'.
+        ///</summary>
+		public string UniqueIdentifier { get; set; }
 
-        public void RemoveBbie(string name)
-        {
-            bbies.RemoveAll(bbie => bbie.Name == name);
-        }
+        ///<summary>
+        /// Tagged value 'versionIdentifier'.
+        ///</summary>
+		public string VersionIdentifier { get; set; }
+
+        ///<summary>
+        /// Tagged value 'usageRule'.
+        ///</summary>
+		public IEnumerable<string> UsageRules { get; set; }
+
+		#endregion
     }
 }
+
--- CctsRepository/BieLibrary/AsbieSpec.cs
+++ CctsRepository/BieLibrary/AsbieSpec.cs
@@ -1,109 +1,86 @@
-using System;
+
+// *******************************************************************************
+// This file is part of the VIENNAAddIn project
+// 
+// Licensed under GNU General Public License V3 http://gplv3.fsf.org/
+// 
+// For further information on the VIENNAAddIn project please visit 
+// http://vienna-add-in.googlecode.com
+// *******************************************************************************
+
 using System.Collections.Generic;
+// ReSharper disable RedundantUsingDirective
+using CctsRepository.BdtLibrary;
+using CctsRepository.BieLibrary;
+using CctsRepository.BLibrary;
 using CctsRepository.CcLibrary;
+using CctsRepository.CdtLibrary;
+using CctsRepository.DocLibrary;
+using CctsRepository.EnumLibrary;
+using CctsRepository.PrimLibrary;
+// ReSharper restore RedundantUsingDirective
 
 namespace CctsRepository.BieLibrary
 {
-    public class AsbieSpec
+    public partial class AsbieSpec
     {
-        private AggregationKind aggregationKind = AggregationKind.Composite;
-        private int associatedABIEId;
-
-        public AsbieSpec(IAsbie asbie)
-        {
-            Name = asbie.Name;
-            DictionaryEntryName = asbie.DictionaryEntryName;
-            Definition = asbie.Definition;
-            UniqueIdentifier = asbie.UniqueIdentifier;
-            VersionIdentifier = asbie.VersionIdentifier;
-            LanguageCode = asbie.LanguageCode;
-            BusinessTerms = new List<string>(asbie.BusinessTerms);
-            UsageRules = new List<string>(asbie.UsageRules);
-            SequencingKey = asbie.SequencingKey;
-            AssociatedABIEId = asbie.AssociatedAbie.Id;
-            LowerBound = asbie.LowerBound;
-            UpperBound = asbie.UpperBound;
-            AggregationKind = asbie.AggregationKind;
-        }
-
-        public AsbieSpec()
-        {
-        }
-
-        public string SequencingKey { get; set; }
-
-        public int AssociatedABIEId
-        {
-            get
-            {
-                if (ResolveAssociatedABIE != null)
-                {
-                    return ResolveAssociatedABIE().Id;
-                }
-                return associatedABIEId;
-            }
-            set { associatedABIEId = value; }
-        }
-
-        /// <summary>
-        /// Set a function to resolve the associated ACC.
-        /// </summary>
-        public Func<IAbie> ResolveAssociatedABIE { get; set; }
-
-        public string LowerBound { get; set; }
+		public string Name { get; set; }
+		
         public string UpperBound { get; set; }
+		
+        public string LowerBound { get; set; }
 
-        public AggregationKind AggregationKind
-        {
-            get { return aggregationKind; }
-            set { aggregationKind = value; }
-        }
+		public AggregationKind AggregationKind { get; set; }
+		
+		public IAbie AssociatingAbie { get; set; }
 
-        public IEnumerable<string> UsageRules { get; set; }
-        public string Name { get; set; }
-        public string DictionaryEntryName { get; set; }
-        public string Definition { get; set; }
-        public string UniqueIdentifier { get; set; }
-        public string VersionIdentifier { get; set; }
-        public string LanguageCode { get; set; }
-        public IEnumerable<string> BusinessTerms { get; set; }
+		public IAbie AssociatedAbie { get; set; }
 
-        public static AsbieSpec CloneASCC(IAscc ascc, string name, int associatedABIEId)
-        {
-            return new AsbieSpec
-                   {
-                       BusinessTerms = new List<string>(ascc.BusinessTerms),
-                       Definition = ascc.Definition,
-                       DictionaryEntryName = ascc.DictionaryEntryName,
-                       LanguageCode = ascc.LanguageCode,
-                       SequencingKey = ascc.SequencingKey,
-                       UniqueIdentifier = ascc.UniqueIdentifier,
-                       UsageRules = new List<string>(ascc.UsageRules),
-                       VersionIdentifier = ascc.VersionIdentifier,
-                       Name = name,
-                       AssociatedABIEId = associatedABIEId,
-                       LowerBound = ascc.LowerBound,
-                       UpperBound = ascc.UpperBound,
-                   };
-        }
+		public IAscc BasedOn { get; set; }
 
-        public static AsbieSpec CloneASCC(IAscc ascc, string name, Func<IAbie> associatedABIEResolver)
-        {
-            return new AsbieSpec
-                   {
-                       BusinessTerms = new List<string>(ascc.BusinessTerms),
-                       Definition = ascc.Definition,
-                       DictionaryEntryName = ascc.DictionaryEntryName,
-                       LanguageCode = ascc.LanguageCode,
-                       SequencingKey = ascc.SequencingKey,
-                       UniqueIdentifier = ascc.UniqueIdentifier,
-                       UsageRules = new List<string>(ascc.UsageRules),
-                       VersionIdentifier = ascc.VersionIdentifier,
-                       Name = name,
-                       ResolveAssociatedABIE = associatedABIEResolver,
-                       LowerBound = ascc.LowerBound,
-                       UpperBound = ascc.UpperBound,
-                   };
-        }
+		#region Tagged Values
+
+        ///<summary>
+        /// Tagged value 'businessTerm'.
+        ///</summary>
+		public IEnumerable<string> BusinessTerms { get; set; }
+
+        ///<summary>
+        /// Tagged value 'definition'.
+        ///</summary>
+		public string Definition { get; set; }
+
+        ///<summary>
+        /// Tagged value 'dictionaryEntryName'.
+        ///</summary>
+		public string DictionaryEntryName { get; set; }
+
+        ///<summary>
+        /// Tagged value 'languageCode'.
+        ///</summary>
+		public string LanguageCode { get; set; }
+
+        ///<summary>
+        /// Tagged value 'sequencingKey'.
+        ///</summary>
+		public string SequencingKey { get; set; }
+
+        ///<summary>
+        /// Tagged value 'uniqueIdentifier'.
+        ///</summary>
+		public string UniqueIdentifier { get; set; }
+
+        ///<summary>
+        /// Tagged value 'versionIdentifier'.
+        ///</summary>
+		public string VersionIdentifier { get; set; }
+
+        ///<summary>
+        /// Tagged value 'usageRule'.
+        ///</summary>
+		public IEnumerable<string> UsageRules { get; set; }
+
+		#endregion
     }
 }
+
