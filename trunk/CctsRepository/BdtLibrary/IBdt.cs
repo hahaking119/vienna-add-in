2f5c716be999fb7d54584035ddeb886ce28ffdbe
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2009-12-10 09:47:03 +0000

    Update issue 117 - Defined CRUD methods for attributes, associations and enumeration literals in all UPCC classifier interfaces. - Defined Remove methods for sub-libraries and elements in library interfaces.

--- trunk/CctsRepository/BdtLibrary/IBdt.cs
+++ trunk/CctsRepository/BdtLibrary/IBdt.cs
@@ -38,6 +38,27 @@ namespace CctsRepository.BdtLibrary
 
 		IEnumerable<IBdtSup> Sups { get; }
 
+		/// <summary>
+		/// Creates a(n) SUP based on the given <paramref name="specification"/>.
+		/// <param name="specification">A specification for a(n) SUP.</param>
+		/// <returns>The newly created SUP.</returns>
+		/// </summary>
+		IBdtSup CreateBdtSup(BdtSupSpec specification);
+
+		/// <summary>
+		/// Updates a(n) SUP to match the given <paramref name="specification"/>.
+		/// <param name="bdtSup">A(n) SUP.</param>
+		/// <param name="specification">A new specification for the given SUP.</param>
+		/// <returns>The updated SUP. Depending on the implementation, this might be the same updated instance or a new instance!</returns>
+		/// </summary>
+        IBdtSup UpdateBdtSup(IBdtSup bdtSup, BdtSupSpec specification);
+
+		/// <summary>
+		/// Removes a(n) SUP from this BDT.
+		/// <param name="bdtSup">A(n) SUP.</param>
+		/// </summary>
+        void RemoveBdtSup(IBdtSup bdtSup);
+
 		#region Tagged Values
 
         ///<summary>
