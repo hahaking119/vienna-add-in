e02fa929b8172d1c66d870a74700e100c63d4f67
cschollum <cschollum@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-01-07 16:43:35 +0000

    Update issue 118 - Finished implementation of enumeration literal templates.

--- VIENNAAddIn/upcc3/uml/IUmlEnumerationLiteral.cs
+++ VIENNAAddIn/upcc3/uml/IUmlEnumerationLiteral.cs
@@ -2,5 +2,8 @@ namespace VIENNAAddIn.upcc3.uml
 {
     public interface IUmlEnumerationLiteral
     {
+        int Id { get; }
+        string Name { get; }
+        IUmlTaggedValue GetTaggedValue(string name);
     }
 }
