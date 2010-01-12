39584acb97a8ccac02aae237c966125767ac4004
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-01-12 13:07:35 +0000

    added TemporaryBdtModelTest

---
+++ trunk/VIENNAAddInUnitTests/upcc3/Wizards/dev/temporarymodel/bdtmodel/TemporaryBdtModelTest.cs
@@ -0,0 +1,288 @@
+﻿// *******************************************************************************
+// This file is part of the VIENNAAddIn project
+// 
+// Licensed under GNU General Public License V3 http://gplv3.fsf.org/
+// 
+// For further information on the VIENNAAddIn project please visit 
+// http://vienna-add-in.googlecode.com
+// *******************************************************************************
+
+using System.Windows.Input;
+using CctsRepository.BdtLibrary;
+using NUnit.Framework;
+using VIENNAAddIn.upcc3.ccts.dra;
+using VIENNAAddIn.upcc3.Wizards.dev.temporarymodel.bdtmodel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CctsRepository;
+using CctsRepository.CdtLibrary;
+using System.Collections.Generic;
+using VIENNAAddIn.upcc3.Wizards.dev.util;
+using VIENNAAddInUnitTests.upcc3.Wizards.dev.TestRepository;
+using VIENNAAddInUtils;
+using Assert=Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace VIENNAAddInUnitTests.upcc3.Wizards.dev.temporarymodel.bdtmodel
+{
+    /// <summary>
+    ///This is a test class for TemporaryBdtModelTest and is intended
+    ///to contain all TemporaryBdtModelTest Unit Tests
+    ///</summary>
+    [TestFixture]
+    public class TemporaryBdtModelTest
+    {
+        private ICctsRepository cctsRepository;
+        private TemporaryBdtModel target;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
+
+        #region Additional test attributes
+        
+        //SetUp runs code before each test
+        [SetUp]
+        public void SetUp()
+        {
+            cctsRepository = new CCRepository(new EARepositoryBdtEditor());
+            target = new TemporaryBdtModel(cctsRepository);
+                var candidateCdtLibraryIndex = 0;
+                target.mCandidateCdtLibraries[candidateCdtLibraryIndex].mCandidateCdts = new List<CandidateCdt>();
+                target.CandidateCdtNames = new List<string>();
+                target.CandidateConItems = new List<CheckableItem>();
+                target.CandidateSupItems = new List<CheckableItem>();
+                foreach (ICdt cdt in cctsRepository.GetCdtLibraryByPath((Path)"test model"/"blib1"/"cdtlib1").Cdts)
+                {
+                    var candidateCdt = new CandidateCdt(cdt);
+                    target.mCandidateCdtLibraries[candidateCdtLibraryIndex].mCandidateCdts.Add(candidateCdt);
+                    target.CandidateCdtNames.Add(cdt.Name);
+
+                    int candidateCdtIndex =
+                        target.mCandidateCdtLibraries[candidateCdtLibraryIndex].mCandidateCdts.IndexOf(candidateCdt);
+                    target.mCandidateCdtLibraries[candidateCdtLibraryIndex].mCandidateCdts[candidateCdtIndex].
+                        mPotentialCon.OriginalCdtCon = cdt.Con;
+                    target.CandidateConItems.Add(new CheckableItem(false,cdt.Con.Name,false,false, Cursors.Arrow));
+                    target.mCandidateCdtLibraries[candidateCdtLibraryIndex].CandidateCdts[candidateCdtIndex].mPotentialSups = new List<PotentialSup>();
+                    foreach (ICdtSup cdtSup in cdt.Sups)
+                    {
+                        target.mCandidateCdtLibraries[candidateCdtLibraryIndex].mCandidateCdts[candidateCdtIndex].mPotentialSups.Add(new PotentialSup(cdtSup));
+                        target.CandidateSupItems.Add(new CheckableItem(false,cdtSup.Name,false,false,Cursors.Arrow));
+                    }
+                }
+        }
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Prefix
+        ///</summary>
+        [Test]
+        public void ShouldSetPrefix()
+        {
+            const string expected = "MyPrefix";
+            target.Prefix = expected;
+            var actual = target.Prefix;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Name
+        ///</summary>
+        [Test]
+        public void ShouldSetName()
+        {
+            const string expected = "MyName";
+            target.Name = expected;
+            var actual = target.Name;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for CandidateSupItems
+        ///</summary>
+        [Test]
+        public void ShouldSetCandidateSupItems()
+        {
+            var expected = new List<CheckableItem> {new CheckableItem(true,"test",false,false,Cursors.Arrow)};
+            target.CandidateSupItems = expected;
+            var actual = target.CandidateSupItems;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for CandidateConItems
+        ///</summary>
+        [Test]
+        public void ShouldSetCandidateConItems()
+        {
+            var expected = new List<CheckableItem> { new CheckableItem(true, "test", false, false, Cursors.Arrow) };
+            target.CandidateConItems = expected;
+            var actual = target.CandidateConItems;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for CandidateCdtNames
+        ///</summary>
+        [Test]
+        public void ShouldSetCandidateCdtNames()
+        {
+            var expected = new List<string>{"test1","test2"};
+            target.CandidateCdtNames = expected;
+            var actual = target.CandidateCdtNames;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for CandidateCdtLibraryNames
+        ///</summary>
+        [Test]
+        public void ShouldSetCandidateCdtLibraryNames()
+        {
+            var expected = new List<string> { "test1", "test2" };
+            target.CandidateCdtLibraryNames = expected;
+            var actual = target.CandidateCdtLibraryNames;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for CandidateBdtLibraryNames
+        ///</summary>
+        [Test]
+        public void ShouldSetCandidateBdtLibraryNames()
+        {
+            var expected = new List<string> { "test1", "test2" };
+            target.CandidateBdtLibraryNames = expected;
+            var actual = target.CandidateBdtLibraryNames;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for setSelectedCandidateCdtLibrary
+        ///</summary>
+        [Test]
+        public void ShouldSelectCandidateCdtLibraryByName()
+        {
+            const string selectedCdtLibrary = "cdtlib1";
+            target.setSelectedCandidateCdtLibrary(selectedCdtLibrary);
+            Assert.IsTrue(target.mCandidateCdtLibraries[0].Selected);
+        }
+
+        /// <summary>
+        ///A test for setSelectedCandidateCdt
+        ///</summary>
+        [Test]
+        public void ShouldSelectCandidateCdtByName()
+        {
+            const string selectedCdtLibrary = "cdtlib1";
+            const string selectedCdt = "Text";
+            target.setSelectedCandidateCdtLibrary(selectedCdtLibrary);
+            target.setSelectedCandidateCdt(selectedCdt);
+            Assert.IsTrue(target.mCandidateCdtLibraries[0].mCandidateCdts[3].Selected);
+        }
+
+        /// <summary>
+        ///A test for setSelectedCandidateBdtLibrary
+        ///</summary>
+        [Test]
+        public void ShouldSelectCandidateBdtLibrary()
+        {
+            const string selectedBdtLibrary = "bdtlib1";
+            target.setSelectedCandidateBdtLibrary(selectedBdtLibrary);
+            Assert.IsTrue(target.mCandidateBdtLibraries[0].Selected);
+        }
+
+        /// <summary>
+        ///A test for setCheckedPotentialSup
+        ///</summary>
+        [Test]
+        public void ShouldSetCheckedPotentialSup()
+        {
+            const bool checkedValue = true;
+            const string selectedSup = "Language";
+            const string selectedCdtLibrary = "cdtlib1";
+            const string selectedCdt = "Text";
+            target.setSelectedCandidateCdtLibrary(selectedCdtLibrary);
+            target.setSelectedCandidateCdt(selectedCdt);
+            target.setCheckedPotentialSup(checkedValue, selectedSup);
+            Assert.IsTrue(target.mCandidateCdtLibraries[0].mCandidateCdts[3].mPotentialSups[0].Checked);
+        }
+
+        /// <summary>
+        ///A test for setCheckedAllPotentialSups
+        ///</summary>
+        [Test]
+        public void ShouldSetCheckedAllPotentialSups()
+        {
+            const string selectedCdtLibrary = "cdtlib1";
+            const string selectedCdt = "Code";
+            const bool checkedValue = true;
+            target.setSelectedCandidateCdtLibrary(selectedCdtLibrary);
+            target.setSelectedCandidateCdt(selectedCdt);
+            target.setCheckedAllPotentialSups(checkedValue);
+            foreach (PotentialSup potentialSup in target.mCandidateCdtLibraries[0].mCandidateCdts[0].mPotentialSups)
+            {
+                Assert.IsTrue(potentialSup.Checked);
+            }
+        }
+
+        /// <summary>
+        ///A test for CreateBdt
+        ///</summary>
+        [Test]
+        public void ShouldCreateBdt()
+        {
+            const string selectedCdtLibrary = "cdtlib1";
+            const string selectedCdt = "Code";
+            const bool checkedValue = true;
+            target.setSelectedCandidateCdtLibrary(selectedCdtLibrary);
+            target.setSelectedCandidateCdt(selectedCdt);
+            target.setCheckedAllPotentialSups(checkedValue);
+            target.Prefix = "MyPrefix";
+            target.Name = "MyName";
+            target.setSelectedCandidateBdtLibrary("bdtlib1");
+
+            target.CreateBdt();
+            var testsuccess = false;
+            foreach (IBdt bdt in cctsRepository.GetBdtLibraryByPath((Path) "test model"/"blib1"/"bdtlib1").Bdts)
+            {
+                if(bdt.Name.Equals("MyPrefixMyName"))
+                {
+                    testsuccess = true;
+                }
+            }
+            Assert.IsTrue(testsuccess);
+        }
+
+        #region unused
+        ///// <summary>
+        /////A test for TemporaryBdtModel Constructor: see no sense in that
+        /////</summary>
+        //[Test()]
+        //public void TemporaryBdtModelConstructorTest()
+        //{
+        //    ICctsRepository cctsRepository = null; // TODO: Initialize to an appropriate value
+        //    TemporaryBdtModel target = new TemporaryBdtModel(cctsRepository);
+        //    Assert.Inconclusive("TODO: Implement code to verify target");
+        //}
+
+        /// <summary>
+        ///A test for Reset: Testing the refresh Method of cccache is not in the scope of TemporaryBdtModelTest see TODO: add reference
+        ///</summary>
+        //[Test]
+        //public void ResetTest()
+        //{
+        //    target.Reset();
+        //    Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        //}
+        #endregion
+    }
+}
