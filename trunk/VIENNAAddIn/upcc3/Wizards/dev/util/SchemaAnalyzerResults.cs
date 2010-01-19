a69fb23e734cae1a061cbecf4b0d01e08ad3c4c2
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-01-19 15:50:22 +0000

    added XMLSchemaReader some improvements

--- trunk/VIENNAAddIn/upcc3/Wizards/dev/util/SchemaAnalyzerResults.cs
+++ trunk/VIENNAAddIn/upcc3/Wizards/dev/util/SchemaAnalyzerResults.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System.Collections.Generic;
 namespace VIENNAAddIn.upcc3.Wizards.dev.util
 {
     class SchemaAnalyzerResults
---
+++ trunk/VIENNAAddInUnitTests/upcc3/Wizards/dev/util/XMLSchemaReaderTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using VIENNAAddIn.upcc3.Wizards.dev.util;
+
+namespace VIENNAAddInUnitTests.upcc3.Wizards.dev.util
+{
+ 
+    [TestFixture]
+    public class XMLSchemaReaderTest
+    {
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [Test]
+        public void XMLSchemaReaderConstructorTest()
+        {
+            new XMLSchemaReader("C:/VIENNAAddIn/VIENNAAddIn/upcc3/import/ebInterface/xsd/Invoice.xsd");
+        }
+    }
+}
