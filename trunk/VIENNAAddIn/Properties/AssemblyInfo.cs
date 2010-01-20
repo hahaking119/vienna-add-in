871ef92994fa0299faf66bd3987f649afce909ad
christian.pichler@researchstudio.at <christian.pichler@researchstudio.at@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-01-20 19:05:49 +0000

    Prepared Vienna Add-In for Installation Package.

--- trunk/VIENNAAddIn/Properties/AssemblyInfo.cs
+++ trunk/VIENNAAddIn/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.6.000")]
+[assembly: AssemblyVersion("0.0.7.000")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
