b677559bd702ded9e388aeeb7376798b49477b26
andi.kuntner <andi.kuntner@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-01-21 11:12:48 +0000

    schema analyzer

--- VIENNAAddIn/upcc3/Wizards/dev/ui/SchemaAnalyzer.xaml.cs
+++ VIENNAAddIn/upcc3/Wizards/dev/ui/SchemaAnalyzer.xaml.cs
@@ -7,6 +7,7 @@
 // http://vienna-add-in.googlecode.com
 // *******************************************************************************
 
+using System.Windows;
 using VIENNAAddIn.menu;
 using VIENNAAddIn.upcc3.Wizards.dev.util;
 using Visifire.Charts;
@@ -17,15 +18,24 @@ namespace VIENNAAddIn.upcc3.Wizards.dev.ui
     {
         private SchemaAnalyzerResults results;
         private bool started = false;
+        private string file1 = "";
+        private string file2 = "";
 
         public SchemaAnalyzer()
         {
             InitializeComponent();
             Chart dummy = new Chart(); // workaround for assembly-error, we will never use this dummy object
-            new SchemaAnalyzerFileSelector("", "", this).ShowDialog();
+            LoadFiles();
         }
 
-        public void SetFiles(string file1, string file2)
+        private void LoadFiles()
+        {
+            fileSelector1.FileName = file1;
+            fileSelector2.FileName = file2;
+            innerCanvas.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void SetFiles()
         {
             if (file1.Length == 0) {
                 FileSelectorCancelled();
@@ -45,7 +55,21 @@ namespace VIENNAAddIn.upcc3.Wizards.dev.ui
             }
         }
 
-        public void FileSelectorCancelled()
+        private void buttonOk_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            file1 = fileSelector1.FileName;
+            file2 = fileSelector2.FileName;
+            SetFiles();
+            innerCanvas.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void buttonClose_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FileSelectorCancelled();
+            innerCanvas.Visibility = System.Windows.Visibility.Collapsed;
+        }
+        
+        private void FileSelectorCancelled()
         {
             if(!started) Close();
         }
@@ -55,18 +79,24 @@ namespace VIENNAAddIn.upcc3.Wizards.dev.ui
             started = true;
             results = XMLSchemaReader.Read(file);
             (chart==1 ? chart1 : chart2).Series[0].DataPoints.Clear();
-            foreach (SchemaAnalyzerResult dataset in results.Items)
+            foreach (SchemaAnalyzerResult dataset in results)
             {
                 DataPoint item = new DataPoint();
                 item.YValue = dataset.Count;
                 item.AxisXLabel = dataset.Caption;
                 (chart == 1 ? chart1 : chart2).Series[0].DataPoints.Add(item);
             }
+            (chart == 1 ? tab1 : tab2).Header = file.Substring(file.LastIndexOf("\\")+1);
         }
 
         public static void ShowForm(AddInContext context)
         {
             new SchemaAnalyzer().ShowDialog();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            LoadFiles();
         }
     }
 }
