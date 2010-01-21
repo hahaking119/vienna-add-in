9fd8a1dce2cab0f58be85428af185e6c1b7d87d8
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-01-21 12:37:21 +0000

    XMLSchemaReader update

--- VIENNAAddIn/upcc3/Wizards/dev/util/SchemaAnalyzerResults.cs
+++ VIENNAAddIn/upcc3/Wizards/dev/util/SchemaAnalyzerResults.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections.Generic;
+﻿// *******************************************************************************
+// This file is part of the VIENNAAddIn project
+// 
+// Licensed under GNU General Public License V3 http://gplv3.fsf.org/
+// 
+// For further information on the VIENNAAddIn project please visit 
+// http://vienna-add-in.googlecode.com
+// *******************************************************************************
+
+using System.Collections.Generic;
 namespace VIENNAAddIn.upcc3.Wizards.dev.util
 {
     public class SchemaAnalyzerResults:List<SchemaAnalyzerResult>
