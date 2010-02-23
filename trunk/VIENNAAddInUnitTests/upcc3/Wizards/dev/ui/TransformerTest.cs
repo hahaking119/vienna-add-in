6d008d7298f7abb7a9149dd3c9fa3963438aef8a
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-02-23 15:51:01 +0000

    minor changes to ExporterForm.xaml added Transformer GUI Prototype added Transformer UI test

---
+++ trunk/VIENNAAddInUnitTests/upcc3/Wizards/dev/ui/TransformerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Windows;
+using NUnit.Framework;
+using VIENNAAddIn.upcc3;
+using VIENNAAddIn.upcc3.Wizards.dev.ui;
+using VIENNAAddInUnitTests.upcc3.Wizards.TestRepository;
+
+namespace VIENNAAddInUnitTests.upcc3.Wizards.dev.ui
+{
+    [TestFixture]
+    public class TransformerTest
+    {
+        [Test]
+        [Ignore]
+        public void ShouldOpenAndPopulateForm()
+        {
+            var t = new Thread(() => new Application().Run(new Transformer(CctsRepositoryFactory.CreateCctsRepository(new EARepositoryModelCreator()))));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+        }
+    }
+}
