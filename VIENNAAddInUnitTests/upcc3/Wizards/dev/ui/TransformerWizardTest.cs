92fc51b8fbc5b22e4b443340d0c7ae2e2fdabc42
andi.kuntner <andi.kuntner@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-03-18 14:08:29 +0000

    added transformer wizard testressources

--- VIENNAAddInUnitTests/upcc3/Wizards/dev/ui/TransformerWizardTest.cs
+++ VIENNAAddInUnitTests/upcc3/Wizards/dev/ui/TransformerWizardTest.cs
@@ -14,7 +14,7 @@ namespace VIENNAAddInUnitTests.upcc3.Wizards.dev.ui
         [Ignore]
         public void ShouldOpenAndPopulateForm()
         {
-            var t = new Thread(() => new Application().Run(new TransformerWizard(CctsRepositoryFactory.CreateCctsRepository(new TemporaryFileBasedRepository(TestUtils.PathToTestResource(@"XSDExporterTest\\transformer\\transforming_ebinterface_to_ubl\\repository_containing_ubl_and_ebinterface_invoice.eap"))))));
+            var t = new Thread(() => new Application().Run(new TransformerWizard(CctsRepositoryFactory.CreateCctsRepository(new TemporaryFileBasedRepository(TestUtils.PathToTestResource(@"TranformerTest\\repository_containing_ubl_and_ebinterface_invoice.eap"))))));
             t.SetApartmentState(ApartmentState.STA);
             t.Start();
             t.Join();
