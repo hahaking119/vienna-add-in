0277e3cc2813fb375a65b765c2a2bf41283a5346
fabian.kromer <fabian.kromer@b8219f6a-bc99-11dd-baed-41e496641fac>
2010-10-07 13:53:01 +0000

    minor changes to code layout / documentation

--- VIENNAAddIn/validator/ValidationMessage.cs
+++ VIENNAAddIn/validator/ValidationMessage.cs
@@ -7,90 +7,58 @@ For further information on the VIENNAAddIn project please visit
 http://vienna-add-in.googlecode.com
 *******************************************************************************/
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace VIENNAAddIn.validator
 {
-    class ValidationMessage
+    internal class ValidationMessage
     {
+        #region errorLevelTypes enum
 
-        public enum errorLevelTypes { INFO, WARN, ERROR };
-        static int messageIDcounter;
+        public enum errorLevelTypes
+        {
+            INFO,
+            WARN,
+            ERROR
+        } ;
 
-        
+        #endregion
+
+        private static int messageIDcounter;
+
+
         /// <summary>
         /// Create a new Validationmessage and generate an automatic messageID
         /// </summary>
         /// <param name="message_"></param>
         /// <param name="messageDetail_"></param>
         /// <param name="affectedView_"></param>
-        /// <param name="affectedElementID_"></param>
-        public ValidationMessage(String message_, String messageDetail_, String affectedView_, errorLevelTypes errorLevel_, int affectedPackageID_)
+        /// <param name="errorLevel_"></param>
+        /// <param name="affectedPackageID_"></param>
+        /// 
+        public ValidationMessage(String message_, String messageDetail_, String affectedView_,
+                                 errorLevelTypes errorLevel_, int affectedPackageID_)
         {
-
-            this.message = message_;
-            this.messageDetail = messageDetail_;
-            this.affectedView = affectedView_;
-            this.errorLevel = errorLevel_;
-            this.affectedPackageID = affectedPackageID_;
-            this.messageID = ++messageIDcounter;
-                      
+            Message = message_;
+            MessageDetail = messageDetail_;
+            AffectedView = affectedView_;
+            ErrorLevel = errorLevel_;
+            AffectedPackageID = affectedPackageID_;
+            MessageID = ++messageIDcounter;
         }
 
 
-        int messageID;
-
-        public int MessageID
-        {
-            get { return messageID; }
-            set { messageID = value; }
-        }
+        public int MessageID { get; set; }
 
 
-        errorLevelTypes errorLevel;
+        public errorLevelTypes ErrorLevel { get; set; }
 
-        public errorLevelTypes ErrorLevel
-        {
-            get { return errorLevel; }
-            set { errorLevel = value; }
-        }
 
-        
-        
-        String message;
+        public String Message { get; set; }
 
-        public String Message
-        {
-            get { return message; }
-            set { message = value; }
-        }
-        String messageDetail;
+        public String MessageDetail { get; set; }
 
-        public String MessageDetail
-        {
-            get { return messageDetail; }
-            set { messageDetail = value; }
-        }
+        public String AffectedView { get; set; }
 
-        String affectedView;
-
-        public String AffectedView
-        {
-            get { return affectedView; }
-            set { affectedView = value; }
-        }
-        int affectedPackageID;
-
-        public int AffectedPackageID
-        {
-            get { return affectedPackageID; }
-            set { affectedPackageID = value; }
-        }
-        
-
-        
-
+        public int AffectedPackageID { get; set; }
     }
 }
